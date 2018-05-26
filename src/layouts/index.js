b94adadd51764c27a2bb4d53ee13eea6bb0c9a20
Drew DeCarme <drewdecarme@gmail.com>
2018-05-26 15:38:39 -0400

    feat(Layouts): Updates layouts to include the logo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
+import logo from '../img/logo.png';
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
-    <div>{children()}</div>
+    <Helmet title="Home | Heather Turano Coaching" />
+    <img
+      src={logo}
+      alt="under construction - heather turano coaching logo"
+    />
+    {/* <Navbar /> */}
+    {/* <div>{children()}</div> */}
   </div>
 )
 
