71eb6957e0223ff70a43013dee605ed57305f90f
Drew DeCarme <drewdecarme@gmail.com>
2018-05-26 16:11:47 -0400

    feat(Layouts Index): Add support for global settings and global styleName resolution

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,10 +5,11 @@ import Helmet from 'react-helmet'
 import Navbar from '../components/Navbar'
 import logo from '../img/logo.png';
 import './all.sass'
+import './index.module.scss';
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Heather Turano Coaching" />
+  <div styleName="container">
+    <Helmet title="Home | Heather Turano Coaching" bodyAttributes={{ class: ''}}/>
     <img
       src={logo}
       alt="under construction - heather turano coaching logo"
