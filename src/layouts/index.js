b881d7e6e17cea4f0d4146ef98e9372aaad1f8d1
Drew DeCarme <drewdecarme@gmail.com>
2018-06-24 08:48:49 -0400

    revert(Layouts): Walk back changes to replace children with placeholder images

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,18 +3,17 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 import Header from "../features/Header";
-import logo from "../img/logo.png";
+
 import styles from "./index.module.scss";
 
 const TemplateWrapper = ({ children, ...restProps }) => (
-  <div styleName="placeholder">
+  <div styleName="main">
     <Helmet
       title="Home | Heather Turano Coaching"
       bodyAttributes={{ class: "" }}
     />
-    <img src={logo} alt="under construction - heather turano coaching logo" />
-    {/* <Header {...restProps} />
-    <div styleName="content">{children()}</div> */}
+    <Header {...restProps} />
+    <div styleName="content">{children()}</div>
   </div>
 );
 
