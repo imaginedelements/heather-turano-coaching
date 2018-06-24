ee7b0b70d200ba78be43fe111ef850606483e16a
Drew DeCarme <drewdecarme@gmail.com>
2018-06-24 01:22:32 -0400

    fix(tags): Remove tags page due to failing graphQL queries

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,49 +2,50 @@ import React from "react";
 import Helmet from "react-helmet";
 import Link from "gatsby-link";
 
-class TagRoute extends React.Component {
-  render() {
-    const posts = this.props.data.allMarkdownRemark.edges;
-    console.log(posts);
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ));
-    const tag = this.props.pathContext.tag;
-    const title = this.props.data.site.siteMetadata.title;
-    const totalCount = this.props.data.allMarkdownRemark.totalCount;
-    const tagHeader = `${totalCount} post${
-      totalCount === 1 ? "" : "s"
-    } tagged with “${tag}”`;
-
-    return (
-      <section className="section">
-        <Helmet title={`${tag} | ${title}`} />
-        <div className="container content">
-          <div className="columns">
-            <div
-              className="column is-10 is-offset-1"
-              style={{ marginBottom: "6rem" }}
-            >
-              <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-              <ul className="taglist">{postLinks}</ul>
-              <p>
-                <Link to="/tags">Browse all tags</Link>
-              </p>
-            </div>
+export const TagRoute = ({
+  pathContext: { tag },
+  data: {
+    allMarkdownRemark: { totalCount, edges },
+    site: {
+      siteMetadata: { title }
+    }
+  }
+}) => {
+  console.log(edges);
+  return (
+    <section className="section">
+      <Helmet title={`${tag} | ${title}`} />
+      <div className="container content">
+        <div className="columns">
+          <div
+            className="column is-10 is-offset-1"
+            style={{ marginBottom: "6rem" }}
+          >
+            <h3 className="title is-size-4 is-bold-light">{`${totalCount} post${
+              totalCount === 1 ? "" : "s"
+            } tagged with “${tag}”`}</h3>
+            <ul className="taglist">
+              {edges.map(post => (
+                <li key={post.node.fields.slug}>
+                  <Link to={post.node.fields.slug}>
+                    <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <p>
+              <Link to="/tags">Browse all tags</Link>
+            </p>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 export default TagRoute;
 
-export const tagRouteQuery = graphql`
+export const tagPageQuery = graphql`
   query TagPage($tag: String) {
     site {
       siteMetadata {
