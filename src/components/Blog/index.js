234aa666dfbebfc2a019a406d4fbfa56e892379c
Drew DeCarme <20549969+drewdecarme@users.noreply.github.com>
2018-06-26 21:13:10 -0400

    feat(BlogTagGroup, BlogTag): Add blog tags and blog tag group and make responsive (#11)

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,3 +1,5 @@
 export { BlogContainer } from "./BlogContainer";
 export { BlogHeader } from "./BlogHeader";
 export { BlogContent } from "./BlogContent";
+export { BlogTagGroup } from "./BlogTagGroup";
+export { BlogTag } from "./BlogTag";
