6d0ba083e1f261ce55ab76bb78318765adabcd02
Drew DeCarme <drewdecarme@gmail.com>
2019-03-14 22:00:57 -0400

    fix(AboutPagePreview): Update schema and preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,12 +2,52 @@ import React from "react";
 import PropTypes from "prop-types";
 import { AboutPageTemplate } from "../../templates/about/about.template";
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
+const AboutPagePreview = ({ entry, widgetFor, getAsset }) => (
   <AboutPageTemplate
-    main={entry.getIn(["data", "main"])}
-    overview={entry.getIn(["data", "overview"])}
-    detail={entry.getIn(["data", "detail"])}
-    callToAction={entry.getIn(["data", "callToAction"])}
+    main={{
+      mainTitle: entry.getIn(["data", "main", "mainTitle"]),
+      mainImage: {
+        imgLocation: getAsset(
+          entry.getIn(["data", "main", "mainImage", "imgLocation"])
+        ),
+        imgAlt: entry.getIn(["data", "main", "mainImage", "imgAlt"]),
+        imgXPosition: entry.getIn([
+          "data",
+          "main",
+          "mainImage",
+          "imgXPosition"
+        ]),
+        imgYPosition: entry.getIn([
+          "data",
+          "main",
+          "mainImage",
+          "imgYPosition"
+        ]),
+        imgHeight: entry.getIn(["data", "main", "mainImage", "imgHeight"])
+      },
+      mainBlurb: entry.getIn(["data", "main", "mainBlurb"])
+    }}
+    overview={{
+      overviewTitle: entry.getIn(["data", "overview", "overviewTitle"]),
+      overviewBlub: entry.getIn(["data", "overview", "overviewBlub"])
+    }}
+    detail={{
+      detailTitle: entry.getIn(["data", "detail", "detailTitle"]),
+      detailAvatarImage: getAsset(
+        entry.getIn(["data", "detail", "detailAvatarImage"])
+      ),
+      detailBlurb: entry.getIn(["data", "overview", "detailBlurb"])
+    }}
+    callToAction={{
+      ctaTitle: entry.getIn(["data", "callToAction", "ctaTitle"]),
+      ctaBlurb: entry.getIn(["data", "callToAction", "ctaBlurb"]),
+      ctaInputPlaceholder: entry.getIn([
+        "data",
+        "callToAction",
+        "ctaInputPlaceholder"
+      ]),
+      ctaButtonLabel: entry.getIn(["data", "callToAction", "ctaButtonLabel"])
+    }}
   />
 );
 
