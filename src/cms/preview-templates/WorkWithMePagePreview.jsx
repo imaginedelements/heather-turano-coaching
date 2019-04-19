38cc1114940fffc67af32d8aa406899ebd735ab1
Drew DeCarme <drewdecarme@gmail.com>
2019-04-19 16:29:22 -0400

    fix(WorkWithMePagePreview): Convert to JS to entry

--- src/cms/preview-templates/WorkWithMePagePreview.jsx
+++ src/cms/preview-templates/WorkWithMePagePreview.jsx
@@ -2,39 +2,49 @@ import React from "react";
 import PropTypes from "prop-types";
 import { WorkWithMePageTemplate } from "../../templates/work-with-me/work-with-me.template";
 
-const WorkWithMePagePreview = ({ entry, getAsset }) => (
-  <WorkWithMePageTemplate
-    main={{
-      title: entry.getIn(["data", "main", "title"]),
-      blurb: entry.getIn(["data", "main", "blurb"]),
-      contactForm: {
-        inputPlaceholder: entry.getIn([
-          "data",
-          "main",
-          "contactForm",
-          "inputPlaceholder"
-        ]),
-        emailPlaceholder: entry.getIn([
-          "data",
-          "main",
-          "contactForm",
-          "emailPlaceholder"
-        ]),
-        buttonLabel: entry.getIn(["data", "main", "contactForm", "buttonLabel"])
-      }
-    }}
-    pillars={{
-      title: entry.getIn(["data", "pillars", "title"]),
-      description: entry.getIn(["data", "pillars", "description"]),
-      pillarList: entry.getIn(["data", "pillars", "pillarList"])
-    }}
-    approach={{
-      title: entry.getIn(["data", "approach", "title"]),
-      body: entry.getIn(["data", "approach", "body"]),
-      image: getAsset(entry.getIn(["data", "approach", "image"]))
-    }}
-  />
-);
+const WorkWithMePagePreview = ({ entry, getAsset }) => {
+  const pillars = entry.getIn(["data", "pillars", "pillarList"]);
+  const pillarList = pillars ? pillars.toJS() : [];
+
+  return (
+    <WorkWithMePageTemplate
+      main={{
+        title: entry.getIn(["data", "main", "title"]),
+        blurb: entry.getIn(["data", "main", "blurb"]),
+        contactForm: {
+          inputPlaceholder: entry.getIn([
+            "data",
+            "main",
+            "contactForm",
+            "inputPlaceholder"
+          ]),
+          emailPlaceholder: entry.getIn([
+            "data",
+            "main",
+            "contactForm",
+            "emailPlaceholder"
+          ]),
+          buttonLabel: entry.getIn([
+            "data",
+            "main",
+            "contactForm",
+            "buttonLabel"
+          ])
+        }
+      }}
+      pillars={{
+        title: entry.getIn(["data", "pillars", "title"]),
+        description: entry.getIn(["data", "pillars", "description"]),
+        pillarList
+      }}
+      approach={{
+        title: entry.getIn(["data", "approach", "title"]),
+        body: entry.getIn(["data", "approach", "body"]),
+        image: getAsset(entry.getIn(["data", "approach", "image"]))
+      }}
+    />
+  );
+};
 
 WorkWithMePagePreview.propTypes = {
   entry: PropTypes.shape({
