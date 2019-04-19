4eb1b53009992febc5f5a8e77ed6f93737a685c1
Drew DeCarme <drewdecarme@gmail.com>
2019-04-19 16:16:52 -0400

    fix(WorkWithMePagePreview): Use correct name key

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,12 +2,12 @@ import CMS from "netlify-cms";
 
 import HomePagePreview from "./preview-templates/HomePagePreview";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
-import WorkWithMePageTemplate from "./preview-templates/WorkWithMePageTemplate";
+import WorkWithMePagePreview from "./preview-templates/WorkWithMePagePreview";
 import BlogPagePreview from "./preview-templates/BlogPagePreview";
 import BlogPostPagePreview from "./preview-templates/BlogPostPagePreview";
 
 CMS.registerPreviewTemplate("home", HomePagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
-CMS.registerPreviewTemplate("services", WorkWithMePageTemplate);
+CMS.registerPreviewTemplate("work-with-me", WorkWithMePagePreview);
 CMS.registerPreviewTemplate("blog", BlogPagePreview);
 CMS.registerPreviewTemplate("blog-post", BlogPostPagePreview);
