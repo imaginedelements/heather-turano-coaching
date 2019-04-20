a50ecda4443c1fad8cbee76fb776efdf840efe61
Drew DeCarme <drewdecarme@gmail.com>
2019-04-19 20:57:27 -0400

    test(BlogPostPagePreview): Add proactive logging

--- src/cms/preview-templates/BlogPostPagePreview.jsx
+++ src/cms/preview-templates/BlogPostPagePreview.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { BlogPostPageTemplate } from "../../templates/blog-post/blog-post.template";
 
-const BlogPostPagePreview = ({ entry, widgetFor }) => (
-  <BlogPostPageTemplate
-    title={entry.getIn(["data", "title"])}
-    description={entry.getIn(["data", "description"])}
-    dateCreated={entry.getIn(["data", "date"])}
-    content={widgetFor("body")}
-    // tags={entry.getIn(["data", "tags"])}
-  />
-);
+const BlogPostPagePreview = ({ entry, widgetFor }) => {
+  const body = widgetFor("body");
+
+  return (
+    <BlogPostPageTemplate
+      title={entry.getIn(["data", "title"])}
+      description={entry.getIn(["data", "description"])}
+      dateCreated={entry.getIn(["data", "date"])}
+      content={body}
+      // tags={entry.getIn(["data", "tags"])}
+    />
+  );
+};
 
 BlogPostPagePreview.propTypes = {
   entry: PropTypes.shape({
