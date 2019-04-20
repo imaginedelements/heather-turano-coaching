bfbb003bf0bcbd9fbbca4e788abece01bb26c169
Drew DeCarme <drewdecarme@gmail.com>
2019-04-20 11:54:04 -0400

    fix(typography): Update casing for typography dir

--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -6,3 +6,4 @@ export { default as Icon } from "./Icon";
 export { default as Label } from "./Label";
 export { default as Text } from "./Text";
 export { default as Markdown } from "./Markdown";
+export { default as Color } from "./Color";
