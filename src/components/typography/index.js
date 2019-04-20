252c16c1dbb0d5fadb1ed0743898bab91e7d62fa
Drew DeCarme <drewdecarme@gmail.com>
2019-04-20 11:51:53 -0400

    fix(Color): Force reset of typography interface fl

--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -6,4 +6,3 @@ export { default as Icon } from "./Icon";
 export { default as Label } from "./Label";
 export { default as Text } from "./Text";
 export { default as Markdown } from "./Markdown";
-export { default as Color } from "./Color";
