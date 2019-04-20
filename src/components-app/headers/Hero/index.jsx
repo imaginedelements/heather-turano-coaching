8a0f411de0bfb70a42bb0f467649b0a3fc2b24f8
Drew DeCarme <drewdecarme@gmail.com>
2019-04-20 14:15:42 -0400

    feat(Hero): Add hero subtitle

--- src/components-app/headers/Hero/index.jsx
+++ src/components-app/headers/Hero/index.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { HeroImage } from "./HeroImage";
-import { Heading } from "../../../components/typography";
+import { Heading, Paragraph } from "../../../components/typography";
 
 import "./index.module.scss";
 
-const Hero = ({ img, alt, title, children }) => (
+const Hero = ({ img, alt, title, subTitle, children }) => (
   <section styleName="hero">
     <HeroImage img={img} alt={alt} />
     <div styleName="top">
       <Heading size="xl" copy={`'${title}'`} color="lightscale-0" />
+      <Paragraph size="xl" color="lightscale-0">
+        {subTitle}
+      </Paragraph>
     </div>
     <div styleName="bottom">
       <div>{children}</div>
@@ -22,6 +25,7 @@ Hero.propTypes = {
   children: PropTypes.any.isRequired,
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired
 };
 
